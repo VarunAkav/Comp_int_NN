bfcff33b1034bd4111b33edbb0844de5773afbcd
Jay Tulshian <tulshianjay@gmail.com>
2022-10-31 15:28:17 +0530

    InputLayer

--- NN_real_extract.py
+++ NN_real_extract.py
@@ -12,13 +12,10 @@ modelPaths = dict()
 for filepath in os.listdir('Models'):
     filename, ext = os.path.splitext(filepath)
     if ext == '.h5':
-        modelPaths[filename] = os.path.join('Models',filepath)
-
+        modelPaths[filename] = os.path.join('Models', filepath)
 
 
 class ModelSummary:
-    
-
 
     def __init__(self) -> None:
         self.class_name = ''
@@ -29,19 +26,20 @@ class ModelSummary:
         self.multiplications = 0
         self.comparisions = 0
         self.connections = 0
-    
+
     def toJson(self):
         return {
-            "class_name" : self.class_name,
-            "name" : self.name,
-            "layers" : self.layers,
-            "neurons" : self.neurons,
-            "additions" : self.additions,
-            "multiplications" : self.multiplications,
-            "comparasions" : self.comparisions,
-            "connections" : self.connections,
+            "class_name": self.class_name,
+            "name": self.name,
+            "layers": self.layers,
+            "neurons": self.neurons,
+            "additions": self.additions,
+            "multiplications": self.multiplications,
+            "comparasions": self.comparisions,
+            "connections": self.connections,
         }
-    
+
+
 class LayerSummary:
     def __init__(self) -> None:
         self.class_name = ''
@@ -52,52 +50,69 @@ class LayerSummary:
         self.multiplications = 0
         self.comparisions = 0
         self.connections = 0
-    
+
     def toJson(self):
         return {
-            "class_name" : self.class_name,
-            "name" : self.name,
-            "shape" : self.shape,
-            "neurons" : self.neurons,
-            "additions" : self.additions,
-            "multiplications" : self.multiplications,
-            "comparasions" : self.comparisions,
-            "connections" : self.connections,
+            "class_name": self.class_name,
+            "name": self.name,
+            "shape": self.shape,
+            "neurons": self.neurons,
+            "additions": self.additions,
+            "multiplications": self.multiplications,
+            "comparasions": self.comparisions,
+            "connections": self.connections,
         }
 
+
 class ModelExtractor:
-    
+
+    layerMethods = dict()
+
     def __init__(self, modelPath):
+        print('something')
         self.model = load_model(modelPath)
-        self.summary = self.extract(self.model,None)
-        
-        
-        self.layerMethods = dict()  # dict should contain methods which will return LayerSummary()
+        self.summary = self.extract(self.model)
+
+        # dict should contain methods which will return LayerSummary()
 
     def extract(self, model) -> ModelSummary:
         modelSummary = ModelSummary()
         modelSummary.class_name = model.__class__.__name__
         modelSummary.name = model.name
-        
-        
+
         for layer in model.layers:
             if layer.__class__.__name__ in ['Sequential', 'Functional']:
                 summary = self.extract()
-                
-                 
+
             else:
                 summary = LayerSummary()
                 if layer.__class__.__name__ in self.layerMethods:
-                    summary = self.layerMethods[layer.__class__.__name__](layer)
+                    summary = self.layerMethods[layer.__class__.__name__](
+                        layer)
 
-            modelSummary.additions +=summary.additions
-            modelSummary.multiplications+=summary.multiplications
-            modelSummary.comparisions+=summary.comparisions
-            modelSummary.connections+=summary.connections
+            modelSummary.additions += summary.additions
+            modelSummary.multiplications += summary.multiplications
+            modelSummary.comparisions += summary.comparisions
+            modelSummary.connections += summary.connections
             modelSummary.layers.append(summary)
-        
+
         return modelSummary
-    
+
+    def InputLayer(self, layer):
+        lsummary = LayerSummary()
+
+        lsummary.class_name = layer.__class__.__name__
+        lsummary.name = layer.name
+        if(type(layer.output_shape) == 'list'):
+            lsummary.shape = [eachoutput[1:]
+                              for eachoutput in layer.output_shape]
+        else:
+            lsummary.shape = layer.output_shape[1:]
+        lsummary.neurons = sum(
+            [reduce(lambda x, y: x*y, lsummary.shape[i]) for i in range(len(lsummary.shape))])
+
+        return lsummary
+
     '''
     def func(layer):
         lsummary = LayerSummary()
@@ -117,5 +132,3 @@ class ModelExtractor:
         return lsummary
     
     '''
-
-
